3695810f43fbcb3386abac36b647dffd8ea1bfd8
toughtiger <santosh.cerdak@gmail.com>
2021-04-03 01:39:56 +0530

    added task module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { CommonModule } from './common/common.module';
 import { User } from './users/entity/user.entity';
+import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [
@@ -28,6 +29,7 @@ import { User } from './users/entity/user.entity';
     }),
     UsersModule,
     CommonModule,
+    TaskModule,
   ],
   controllers: [],
   providers: [],
--- src/restaurents/restaurents.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RestaurentResolver } from './restaurents.resolver';
-
-@Module({
-  providers: [RestaurentResolver],
-})
-export class RestaurentsModule {}
